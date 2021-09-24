1bbfa78c7d34e654e63fb359bfcc922fd338cc1a
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2021-09-24 23:35:08 +0200

    Polyfill 64-bit atomics.

--- ci.sh
+++ ci.sh
@@ -7,3 +7,6 @@ cargo build --target thumbv6m-none-eabi
 cargo build --target thumbv7em-none-eabi
 cargo build --target riscv32imc-unknown-none-elf
 cargo build --target riscv32imac-unknown-none-elf
+cargo build --target i686-unknown-linux-gnu
+cargo build --target x86_64-unknown-linux-gnu
+cargo build --target riscv64gc-unknown-linux-gnu
