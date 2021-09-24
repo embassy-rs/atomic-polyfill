5aedd2e7df9d9e9c3db584ab4d096339eb6ea8d8
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2021-09-24 20:33:02 +0200

    Add riscv to CI

--- ci.sh
+++ ci.sh
@@ -5,3 +5,5 @@ set -euxo pipefail
 cargo build
 cargo build --target thumbv6m-none-eabi
 cargo build --target thumbv7em-none-eabi
+cargo build --target riscv32imc-unknown-none-elf
+cargo build --target riscv32imac-unknown-none-elf
