11001a22f9ceb95aaf93e368d545941d63a5ed6e
Scott Mabin <scott@mabez.dev>
2021-09-24 13:25:22 +0100

    Make riscv detection generic, any target _without_ the a extension will be polyfilled.

--- build.rs
+++ build.rs
@@ -1,17 +1,27 @@
+use riscv_target::Target;
 use std::env;
 
 fn main() {
     let target = env::var("TARGET").unwrap();
 
-    let want_polyfill_cas =
-        target.starts_with("thumbv6m-") || target == "riscv32imc-unknown-none-elf";
+    let want_polyfill_cas = target.starts_with("thumbv6m-") || is_riscv_without_atomic_ext(&target);
 
     if want_polyfill_cas {
         println!("cargo:rustc-cfg=polyfill");
     }
 
-    let want_atomic_polyfill = target == "riscv32imc-unknown-none-elf";
+    let want_atomic_polyfill = is_riscv_without_atomic_ext(&target);
     if want_atomic_polyfill {
         println!("cargo:rustc-cfg=polyfill_types");
+    }
+}
+
+fn is_riscv_without_atomic_ext(target: &str) -> bool {
+    if target.starts_with("riscv") {
+        let target = Target::from_target_str(&target);
+
+        !target.has_extension('a')
+    } else {
+        false
     }
 }
