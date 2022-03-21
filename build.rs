df682e6ec83a7efc03c2be4f87545889d808943c
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2022-03-22 00:58:14 +0100

    Add xtensa targets, make unknown targets reexport core::atomic as-is.

--- build.rs
+++ build.rs
@@ -1,4 +1,3 @@
-use riscv_target::Target;
 use std::env;
 use std::fmt;
 
@@ -28,40 +27,56 @@ fn main() {
 
     use PolyfillLevel::*;
 
-    let (level, level64) = if is_riscv_without_atomic_ext(&target) {
-        (Full, Full)
-    } else if target.starts_with("riscv32") {
-        (Native, Full)
-    } else if target.starts_with("thumbv4") {
-        (Full, Full)
-    } else if target.starts_with("thumbv6m-") {
-        (Cas, Full)
-    } else if target.starts_with("thumb") {
-        (Native, Full)
-    } else {
-        (Native, Native)
-    };
+    let patterns = [
+        ("riscv32imac-*", (Native, Full)),
+        ("riscv32gc-*", (Native, Full)),
+        ("riscv32imc-*-espidf", (Native, Native)),
+        ("riscv32*", (Full, Full)),
+        ("avr-*", (Full, Full)),
+        ("thumbv4t-*", (Full, Full)),
+        ("thumbv6m-*", (Cas, Full)),
+        ("thumbv7m-*", (Native, Full)),
+        ("thumbv7em-*", (Native, Full)),
+        ("thumbv8m.base-*", (Native, Full)),
+        ("thumbv8m.main-*", (Native, Full)),
+        ("xtensa-*-espidf", (Native, Native)),
+        ("xtensa-esp32-*", (Native, Full)),
+        ("xtensa-esp32s2-*", (Full, Full)),
+        ("xtensa-esp32s3-*", (Native, Full)),
+        ("xtensa-esp8266-*", (Cas, Full)),
+    ];
 
-    println!("cargo:rustc-cfg=u8_{}", level);
-    println!("cargo:rustc-cfg=u16_{}", level);
-    println!("cargo:rustc-cfg=u32_{}", level);
-    println!("cargo:rustc-cfg=u64_{}", level64);
-    println!("cargo:rustc-cfg=usize_{}", level);
-    println!("cargo:rustc-cfg=i8_{}", level);
-    println!("cargo:rustc-cfg=i16_{}", level);
-    println!("cargo:rustc-cfg=i32_{}", level);
-    println!("cargo:rustc-cfg=i64_{}", level64);
-    println!("cargo:rustc-cfg=isize_{}", level);
-    println!("cargo:rustc-cfg=ptr_{}", level);
-    println!("cargo:rustc-cfg=bool_{}", level);
+    if let Some((_, (level, level64))) = patterns
+        .iter()
+        .find(|(pattern, _)| matches(pattern, &target))
+    {
+        println!("cargo:rustc-cfg=u8_{}", level);
+        println!("cargo:rustc-cfg=u16_{}", level);
+        println!("cargo:rustc-cfg=u32_{}", level);
+        println!("cargo:rustc-cfg=u64_{}", level64);
+        println!("cargo:rustc-cfg=usize_{}", level);
+        println!("cargo:rustc-cfg=i8_{}", level);
+        println!("cargo:rustc-cfg=i16_{}", level);
+        println!("cargo:rustc-cfg=i32_{}", level);
+        println!("cargo:rustc-cfg=i64_{}", level64);
+        println!("cargo:rustc-cfg=isize_{}", level);
+        println!("cargo:rustc-cfg=ptr_{}", level);
+        println!("cargo:rustc-cfg=bool_{}", level);
+    } else {
+        // If we don't know about the target, just reexport the entire `core::atomic::*`
+        // This doesn't polyfill anything, but it's guaranteed to never fail build.
+        println!("cargo:rustc-cfg=reexport_core");
+    }
 }
 
-fn is_riscv_without_atomic_ext(target: &str) -> bool {
-    if target.starts_with("riscv") {
-        let target = Target::from_target_str(&target);
-
-        !target.has_extension('a')
+// tiny glob replacement to avoid pulling in more crates.
+// Supports 0 or 1 wildcards `*`
+fn matches(pattern: &str, val: &str) -> bool {
+    if let Some(p) = pattern.find('*') {
+        let prefix = &pattern[..p];
+        let suffix = &pattern[p + 1..];
+        val.len() >= prefix.len() + suffix.len() && val.starts_with(prefix) && val.ends_with(suffix)
     } else {
-        false
+        val == pattern
     }
 }
